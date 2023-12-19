0df72e53e75a16cea399d109d2af46cb95508d1d
Shanab <mostafashanaab@gmail.com>
2023-12-19 04:36:59 +0200

    Solution of Task 4

--- CodeRefactoring/Task4.cs
+++ CodeRefactoring/Task4.cs
@@ -9,6 +9,10 @@ namespace CodeRefactoring
         /// </summary>
         public static int Fibonacci(int n)
         {
+            if (n <= 0)
+            {
+                throw new ArgumentException("Input is Negative !", nameof(n));
+            }
             if (n <= 2)
             {
                 return 1;
@@ -24,7 +28,7 @@ namespace CodeRefactoring
                 current = next;
             }
 
-            return prev;
+            return current;
         }
     }
 }
